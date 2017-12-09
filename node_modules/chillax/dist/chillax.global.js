5a6e1f5cc67d73dbf84274c851460360772e6b3c
Chase Moskal <chasemoskal@gmail.com>
2017-12-08 18:54:39 -0800

    fix root gitignore of '/dist'

---
+++ node_modules/chillax/dist/chillax.global.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var chillax_1 = require("./chillax");
+window["Chillax"] = chillax_1.default;
+//# sourceMappingURL=chillax.global.js.map
